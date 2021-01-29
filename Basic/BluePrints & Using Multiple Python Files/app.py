0a30503bc63d626dcdc87b7bcf63055fdccaf79d
amit998 <damitlucky998@gmail.com>
2021-01-29 18:00:04 +0530

    added

--- Basic/BluePrints & Using Multiple Python Files/app.py
+++ Basic/BluePrints & Using Multiple Python Files/app.py
@@ -1,14 +1,15 @@
 from flask import Flask,render_template
+from admin.second import second
 
 
 app=Flask(__name__)
-
+app.register_blueprint(second,url_prefix="/admin")
 
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+def test():
+    return "<h1>Hi</h1>"
+
 
 
 if(__name__=="__main__"):
